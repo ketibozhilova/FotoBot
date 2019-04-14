b2061e23e9cf552f266a27fb73e645a529cfb90d
Keti <keti.bozhilova@outlook.de>
2019-04-14 22:51:20 +0200

    kamera bearbeitet
    weiter bearbeitet

---
+++ autostartcamera.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 20:29:40 2019
+
+@author: kejt
+"""
+
--- camerafoto.py
+++ camerafoto.py
@@ -1,9 +1,13 @@
 from picamera import PiCamera
 from time import sleep
 
-camera = PiCamera()
 
-camera.start_preview()
-sleep(5)
-camera.capture('/home/pi/Pictures/Aufnahme/image.jpg')
-camera.stop_preview()
+def takeapicture():
+
+    camera = PiCamera()
+
+    camera.start_preview()
+    sleep(5)
+    camera.capture('/home/pi/Pictures/Aufnahme/image.jpg')
+    camera.stop_preview()
+
