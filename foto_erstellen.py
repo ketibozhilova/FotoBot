faa5816e49c7e3b1a4a337fb27f46c179dd59746
Andreas Mings <andreas.mings@hotmail.com>
2019-04-19 17:27:25 +0200

    camerafoto --> foto_erstellen foto_erstellen derzeit kein Modul

---
+++ foto_erstellen.py
@@ -0,0 +1,13 @@
+#def takeapicture():
+from picamera import PiCamera
+from time import sleep
+import Testmodul
+
+camera = PiCamera()
+
+camera.start_preview()
+sleep(3)
+camera.capture('/home/pi/Pictures/aufnahme/image.jpg')
+camera.stop_preview()
+
+Testmodul.verarbeite()
