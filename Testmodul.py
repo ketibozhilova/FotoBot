16fd12f1cd73e156b4ce47d373fb8f2cc30d89cf
Andreas Mings <andreas.mings@hotmail.com>
2019-04-19 16:40:10 +0200

    experimentiert

--- Testmodul.py
+++ Testmodul.py
@@ -1,7 +1,9 @@
-import pathlib
-import shutil
-
 def verarbeite():
+
+    import pathlib
+    import shutil
+
+
     orig_file = pathlib.Path("/home/pi/Pictures/Aufnahme/image.jpg")
     copyto_file = pathlib.Path("/home/pi/Pictures/original/orig.jpg")
     orig_file.rename(copyto_file)
--- camerafoto.py
+++ camerafoto.py
@@ -1,15 +1,14 @@
-from picamera import PiCamera
-from time import sleep
-import Testmodul
-
 def takeapicture():
+
+    from picamera import PiCamera
+    from time import sleep
+    import Testmodul
 
     camera = PiCamera()
 
     camera.start_preview()
     sleep(3)
-    camera.capture('/home/pi/Pictures/Aufnahme/image.jpg')
+    camera.capture('/home/pi/Pictures/aufnahme/image.jpg')
     camera.stop_preview()
 
-
-Testmodul.verarbeite()
+    Testmodul.verarbeite()
