6de435f8400a7b62d194d8dcc66bde7ad5fe238f
Andreas Mings <andreas.mings@hotmail.com>
2019-04-19 20:10:11 +0200

    Datei zähler hinzugefügt funktioniert noch nicht

--- autostartcamera.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 14 20:29:40 2019
-
-@author: kejt
-"""
-
--- saveOrigPicture.py
+++ saveOrigPicture.py
@@ -1,9 +1,12 @@
 def verarbeite():
 
     import pathlib
-    import shutil
-
-
+    import os
+   
+    pfad = "/home/pi/Pictures/original/"
+    dateien = os.listdir(pfad)
+    anzahl = len(dateien)
+            
     orig_file = pathlib.Path("/home/pi/Pictures/aufnahme/image.jpg")
-    copyto_file = pathlib.Path("/home/pi/Pictures/original/orig.jpg")
+    copyto_file = pathlib.Path("/home/pi/Pictures/original/orig" + str(anzahl + 1) + ".jpg")
     orig_file.rename(copyto_file)
