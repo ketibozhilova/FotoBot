dcc8109d26f6fa911e9ba8a70e4ad7f65d3bc6c7
Andreas Mings <andreas.mings@hotmail.com>
2019-04-22 18:31:37 +0200

    Es funktioniert: - merge-to-print - printing - saveOrigPicture
    wenn beide filesystemwatcher laufen, so passiert folgendes:
    - das bild wird gemacht
    - verarbeitet und in /original/original.jpg verschoben
    - wenn ein weiteres Bild mit dem Namen "erhalten.jpg" in den Ordner
    /erhalten/ kommt, so werden die Bilder:
    /original/original.jpg und /erhalten/abgeholt.jpg
    zusammengeführt und in den Ordner /save/ verschoben
    - alle Bilder welche gemacht oder bearbeitet wurden werden gelöscht
    - es bleibt nur noch das bild in /save/save-x.jpg

--- merge_to_print-fsw.py
+++ merge_to_print-fsw.py
@@ -2,10 +2,11 @@ import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import saveOrigPicture
-
+import os
 import numpy as np
 import PIL
 from PIL import Image
+import printing
 
 
 class Watcher:
@@ -37,31 +38,54 @@ class Handler(FileSystemEventHandler):
 
         elif event.event_type == 'created':
             # Take any action here when a file is first created.
+                            
             print ("Received created event - %s." % event.src_path)
 
         elif event.event_type == 'modified':
-            # Taken any action here when a file is modified.
+           # Taken any action here when a file is modified.
+            if  os.path.exists('/home/pi/Pictures/original/original.jpg'):
+                list_im = ['/home/pi/Pictures/original/original.jpg', '/home/pi/Pictures/erhalten/abgeholt.jpg']
+                imgs    = [ PIL.Image.open(i) for i in list_im ]
 
-            list_im = ['/home/pi/Pictures/original/orig1.jpg', '/home/pi/Pictures/original/orig2.jpg']
-            imgs    = [ PIL.Image.open(i) for i in list_im ]
+                 # pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
+                min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
+                imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
 
-            # pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
-            min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
-            imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
+                 # save that beautiful picture
+                #imgs_comb = PIL.Image.fromarray( imgs_comb)
+                #imgs_comb.save( '/home/pi/Pictures/merge-to-print/Trifecta.jpg' )    
 
-            # save that beautiful picture
-            #imgs_comb = PIL.Image.fromarray( imgs_comb)
-            #imgs_comb.save( '/home/pi/Pictures/merge-to-print/Trifecta.jpg' )    
+                 # for a vertical stacking it is simple: use vstack
+                imgs_comb = np.vstack( (np.asarray(i.resize(min_shape) ) for i in imgs ) )
+                imgs_comb = PIL.Image.fromarray(imgs_comb)
+                imgs_comb.save('/home/pi/Pictures/merge-to-print/merge.jpg')
 
-            # for a vertical stacking it is simple: use vstack
-            imgs_comb = np.vstack( (np.asarray(i.resize(min_shape) ) for i in imgs ) )
-            imgs_comb = PIL.Image.fromarray(imgs_comb)
-            imgs_comb.save('/home/pi/Pictures/merge-to-print/Trifecta.jpg')
+                pathtosave = '/home/pi/Pictures/merge-to-print/merge.jpg'
+                printing.toprint(pathtosave)
 
-            print ("Received modified event - %s." % event.src_path)
+                print ("Received modified event - %s." % event.src_path)
+            else: print("merge.jpg not found")
+
             
-    
 
 if __name__ == '__main__':
     w = Watcher()
     w.run()
+
+
+
+      #list_im = ['/home/pi/Pictures/original/orig1.jpg', '/home/pi/Pictures/original/orig2.jpg']
+                #imgs    = [ PIL.Image.open(i) for i in list_im ]
+
+                ## pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
+                #min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
+                #imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
+
+                ## save that beautiful picture
+                ##imgs_comb = PIL.Image.fromarray( imgs_comb)
+                ##imgs_comb.save( '/home/pi/Pictures/merge-to-print/Trifecta.jpg' )    
+
+                ## for a vertical stacking it is simple: use vstack
+                #imgs_comb = np.vstack( (np.asarray(i.resize(min_shape) ) for i in imgs ) )
+                #imgs_comb = PIL.Image.fromarray(imgs_comb)
+                #imgs_comb.save('/home/pi/Pictures/merge-to-print/Trifecta.jpg')
