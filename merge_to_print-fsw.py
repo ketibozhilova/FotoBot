71e1e59f48c9d41a44d212784d0acc5ee2037672
Andreas Mings <andreas.mings@hotmail.com>
2019-04-22 16:34:32 +0200

    FileSystemWatcher "merge_to_print-fsw" funktioniert drucken funktioniert

--- merge_to_print-fsw.py
+++ merge_to_print-fsw.py
@@ -3,9 +3,13 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import saveOrigPicture
 
+import numpy as np
+import PIL
+from PIL import Image
+
 
 class Watcher:
-    DIRECTORY_TO_WATCH = "/home/pi/Pictures/aufnahme/"
+    DIRECTORY_TO_WATCH = "/home/pi/Pictures/erhalten/"
 
     def __init__(self):
         self.observer = Observer()  #beobachter
@@ -37,10 +41,26 @@ class Handler(FileSystemEventHandler):
 
         elif event.event_type == 'modified':
             # Taken any action here when a file is modified.
-            saveOrigPicture.verarbeite()
+
+            list_im = ['/home/pi/Pictures/original/orig1.jpg', '/home/pi/Pictures/original/orig2.jpg']
+            imgs    = [ PIL.Image.open(i) for i in list_im ]
+
+            # pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
+            min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
+            imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
+
+            # save that beautiful picture
+            #imgs_comb = PIL.Image.fromarray( imgs_comb)
+            #imgs_comb.save( '/home/pi/Pictures/merge-to-print/Trifecta.jpg' )    
+
+            # for a vertical stacking it is simple: use vstack
+            imgs_comb = np.vstack( (np.asarray(i.resize(min_shape) ) for i in imgs ) )
+            imgs_comb = PIL.Image.fromarray(imgs_comb)
+            imgs_comb.save('/home/pi/Pictures/merge-to-print/Trifecta.jpg')
+
             print ("Received modified event - %s." % event.src_path)
             
-
+    
 
 if __name__ == '__main__':
     w = Watcher()
--- printing.py
+++ printing.py
@@ -9,24 +9,3 @@ printers = conn.getPrinters ()
 prin = conn.getDefault()
 myfile = "/home/pi/Pictures/aufnahme/test.txt"
 conn.printFile (prin, myfile, "Project Report", {})
-
-## Set up CUPS
-#conn = cups.Connection()
-#printers = conn.getPrinters()
-#printer_name = printers.keys()[0]
-#cups.setUser('pi')
-
-## Image (code taken from boothcam.py)
-#im = Image.new('RGBA', (683, 384))
-#im.paste(Image.open('/home/pi/Pictures/aufnahme/image.jpg').resize((683, 384)), ( 0, 0, 683, 384))
-
-## Save data to a temporary file
-#output = mktemp(prefix='jpg')
-#im.save(output, format='jpeg')
-
-## Send the picture to the printer
-#print_id = conn.printFile(printer_name, output, "Photo Booth", {})
-## Wait until the job finishes
-#while conn.getJobs().get(print_id, None):
-#    sleep(1)
-#unlink(output)
